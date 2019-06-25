f401235115f178173b9d27986d02266b6b3aa2aa
Patryk Topolski <venirro@gmail.com>
2019-06-25 14:27:59 +0200

    added field to user, changed all inherit types.

--- src/main/java/model/users/Admin.java
+++ src/main/java/model/users/Admin.java
@@ -8,8 +8,8 @@ public class Admin extends User {
     private List<Codecooler> codecoolerList;
     private List<Integer> listOfLevels;
 
-    public Admin(int id, String login, String password, String firstName, String lastName, int phoneNum, String adress, List<Mentor> mentorList, List<Codecooler> codecoolerList, List<Integer> listOfLevels) {
-        super(id, login, password, firstName, lastName, phoneNum, adress);
+    public Admin(int id, String login, String password, String firstName, String lastName, int phoneNum, String adress, String userType, List<Mentor> mentorList, List<Codecooler> codecoolerList, List<Integer> listOfLevels) {
+        super(id, login, password, firstName, lastName, phoneNum, adress, userType);
         this.mentorList = mentorList;
         this.codecoolerList = codecoolerList;
         this.listOfLevels = listOfLevels;
--- src/main/java/model/users/Mentor.java
+++ src/main/java/model/users/Mentor.java
@@ -5,8 +5,8 @@ package model.users;
 public class Mentor extends User {
     private int roomID;
 
-    public Mentor(int id, String login, String password, String firstName, String lastName, int phoneNum, String adress, int roomID) {
-        super(id, login, password, firstName, lastName, phoneNum, adress);
+    public Mentor(int id, String login, String password, String firstName, String lastName, int phoneNum, String adress, String userType, int roomID) {
+        super(id, login, password, firstName, lastName, phoneNum, adress, userType);
         this.roomID = roomID;
     }
 
