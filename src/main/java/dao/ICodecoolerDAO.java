9956e581b14629d8f7cdc7e5abffecee37a1eadc
Stefania Hrycyk Kubat <stefania.hrycyk@gmail.com>
2019-06-26 13:40:45 +0200

    ADD: show ammount of coins of student, select by ID

--- src/main/java/dao/ICodecoolerDAO.java
+++ src/main/java/dao/ICodecoolerDAO.java
@@ -1,8 +1,14 @@
 package  dao;
 
 
+import model.items.Artifact;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public interface ICodecoolerDAO{
-    int showWallet();
+    int showWallet(int id) throws SQLException;
+    List<Artifact> showBoughtAtifacts(int id) throws SQLException;
     void buyArtifact();
     void buyArtifactWithTeammates();
     void showLevelOfExperience();
--- src/main/java/dao/codecoolerDAO.java
+++ src/main/java/dao/codecoolerDAO.java
@@ -1,9 +1,58 @@
 package dao;
 
+import model.items.Artifact;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class codecoolerDAO implements ICodecoolerDAO {
-    public int showWallet() {
-        return 0;
+    DBCreator dbCreator = new DBCreator();
+
+    public int showWallet(int id) throws SQLException {
+        Connection con = dbCreator.connectToDatabase();
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        int ammountOfCoins = 0;
+
+        try {
+            stmt = con.prepareStatement("SELECT coolcoins FROM studentpersonals WHERE user_id = ?;");
+            stmt.setInt(1, id);
+            rs = stmt.executeQuery();
+
+            while(rs.next()){
+                ammountOfCoins = rs.getInt("coolcoins");
+            }
+            stmt.close();
+            con.close();
+        } catch ( Exception e ) {
+            System.out.println(e);
+        }
+        return ammountOfCoins;
     }
+
+//    public List<Artifact> showBoughtAtifacts(int id) throws SQLException{
+//        Connection con = dbCreator.connectToDatabase();
+//        PreparedStatement stmt = null;
+//        ResultSet rs = null;
+//        List<Artifact> boughtArtifacts = new ArrayList<>();
+//
+//
+//        try {
+//            stmt = con.prepareStatement("SELECT  FROM studentpersonals WHERE user_id = ?;");
+//            stmt.setInt(1, id);
+//            rs = stmt.executeQuery();
+//
+//            while(rs.next()){
+//                = rs.getInt("coolcoins");
+//            }
+//            stmt.close();
+//            con.close();
+//        } catch ( Exception e ) {
+//            System.out.println(e);
+//        }
+//        return boughtArtifacts;
+//    }
 
     public void buyArtifact() {
 
