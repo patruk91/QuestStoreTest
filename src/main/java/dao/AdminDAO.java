d8ae769ddc822c6914afdbd8b17de896129d293a
Patryk Topolski <venirro@gmail.com>
2019-06-26 13:59:22 +0200

    fixed query to get mentor and information about class.

--- src/main/java/dao/AdminDAO.java
+++ src/main/java/dao/AdminDAO.java
@@ -24,7 +24,8 @@ public class AdminDAO implements IAdminDAO {
 
     public Mentor getMentor(int id) throws SQLException {
         Connection connection = dbCreator.connectToDatabase();
-        PreparedStatement stm = connection.prepareStatement("SELECT * FROM mentorspersonals , classes WHERE user_id = ?");
+        PreparedStatement stm = connection.prepareStatement("select * from mentorspersonals left join classes on " +
+                "mentorspersonals.user_id = classes.user_id where mentorspersonals.user_id = ?;");
 
         stm.setInt(1, id);
         ResultSet result = stm.executeQuery();
@@ -50,7 +51,40 @@ public class AdminDAO implements IAdminDAO {
 
     }
 
-    public Mentor getMentor(String firstName, String lastName) {
+    public Mentor getMentor(String firstName, String lastName) throws SQLException {
+        Connection connection = dbCreator.connectToDatabase();
+        PreparedStatement stm = connection.prepareStatement("SELECT * FROM mentorspersonals WHERE first_name = ? AND last_name = ?");
+
+        stm.setString(1, firstName);
+        stm.setString(2, lastName);
+        ResultSet result = stm.executeQuery();
+
+
+        int userID = 0;
+        if(result.next()){
+            userID = result.getInt("user_id");
+        }
+
+        stm = connection.prepareStatement("select * from mentorspersonals left join classes on " +
+                "mentorspersonals.user_id = classes.user_id where mentorspersonals.user_id = ?;");
+        stm.setInt(1, userID);
+
+        result = stm.executeQuery();
+
+
+        while (result.next()){
+            int mentorID = result.getInt("user_id");
+            String phoneNum = result.getString("phone_number");
+            String email = result.getString("email");
+            String address = result.getString("address");
+            int classID = result.getInt("class_id");
+            Mentor mentor = new Mentor(mentorID, firstName, lastName, phoneNum, email,address, classID);
+            return mentor;
+        }
+
+
+        connection.close();
+
         return null;
     }
 
