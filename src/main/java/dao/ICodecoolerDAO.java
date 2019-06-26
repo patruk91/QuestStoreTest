af2e720cff9a3bfa388b2162905b101619453585
Stefania Hrycyk Kubat <stefania.hrycyk@gmail.com>
2019-06-26 14:37:03 +0200

    ADD: show all artifacts bought by student by his ID, new constructor for Artifact

--- src/main/java/dao/ICodecoolerDAO.java
+++ src/main/java/dao/ICodecoolerDAO.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public interface ICodecoolerDAO{
     int showWallet(int id) throws SQLException;
-    List<Artifact> showBoughtAtifacts(int id) throws SQLException;
+    List<Artifact> showBoughtArtifacts(int id) throws SQLException;
     void buyArtifact();
     void buyArtifactWithTeammates();
     void showLevelOfExperience();
