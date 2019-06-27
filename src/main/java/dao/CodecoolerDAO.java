0e593e95915e363263c9b6045feecb447779af19
Patryk Topolski <venirro@gmail.com>
2019-06-27 09:52:37 +0200

    statments to check if user can buy artifact, and update his money created.

--- src/main/java/dao/CodecoolerDAO.java
+++ src/main/java/dao/CodecoolerDAO.java
@@ -1,5 +1,7 @@
 package dao;
 
+import org.postgresql.core.SqlCommand;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -22,7 +24,13 @@ public class CodecoolerDAO implements ICodecoolerDAO {
         return 0;
     }
 
-    public void buyArtifact() {
+    public void buyArtifact(int userID, int artifactID)throws SQLException {
+        boolean checkIFcanBeBought = checkIfTransactionPossible(userID, artifactID);
+
+        while (checkIFcanBeBought){
+            updateMoneyAmount(userID,artifactID);
+            addArtifactToItems(userID, artifactID);
+        }
 
     }
 
@@ -32,5 +40,69 @@ public class CodecoolerDAO implements ICodecoolerDAO {
 
     public void showLevelOfExperience() {
 
+    }
+
+    private void updateMoneyAmount(int userID, int artifactID) throws SQLException{
+        int cost = getArtifactCost(artifactID);
+        DBCreator dbCreator = new DBCreator();
+        Connection connection = dbCreator.connectToDatabase();
+
+        int coolcoins = getCoolCoins(userID);
+        int artifactCost = getArtifactCost(artifactID);
+        int restCoins = coolcoins = artifactCost;
+
+        PreparedStatement stm = connection.prepareStatement("update  studentpersonals set coolcoins = ? where user_id = ?");
+        stm.setInt(1, restCoins);
+        stm.setInt(2, userID);
+
+
+
+
+    }
+
+    //todo
+    private void addArtifactToItems(int userID, int artifactID) throws SQLException{}
+
+    private boolean checkIfTransactionPossible(int userID, int artifactID) throws SQLException {
+
+        int coolcoins = getCoolCoins(userID);
+
+        int artifactCost = getArtifactCost(artifactID);
+
+
+        System.out.println(coolcoins >= artifactCost);
+        if (coolcoins >= artifactCost){return true;}
+        return false;
+    }
+
+
+    private int getCoolCoins(int userID) throws SQLException{
+        DBCreator dbCreator = new DBCreator();
+        Connection connection = dbCreator.connectToDatabase();
+        PreparedStatement stm = connection.prepareStatement("select coolcoins from studentpersonals where user_id = ? ");
+        stm.setInt(1, userID);
+        ResultSet result = stm.executeQuery();
+        int coolcoins = 0;
+        if (result.next()){
+            coolcoins = result.getInt("coolcoins");
+        }
+        System.out.println(coolcoins);
+        connection.close();
+        return coolcoins;
+    }
+    private int getArtifactCost(int artifactID) throws SQLException{
+        DBCreator dbCreator = new DBCreator();
+        Connection connection = dbCreator.connectToDatabase();
+        PreparedStatement stm = connection.prepareStatement("select artifact_price from artifacts where  id = ? ");
+        stm = connection.prepareStatement("select artifact_price from artifacts where  id = ? ");
+        stm.setInt(1, artifactID);
+        ResultSet result = stm.executeQuery();
+        int artifactCost = 0;
+        if (result.next()){
+            artifactCost = result.getInt("artifact_price");
+        }
+        System.out.println(artifactCost);
+        connection.close();
+        return artifactCost;
     }
 }
