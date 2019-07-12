021cbef182d220854ea4ad235d039504b38ad76e
Stefania Hrycyk Kubat <stefania.hrycyk@gmail.com>
2019-07-12 13:23:08 +0200

    merge commit

--- src/main/java/controller/MentorController.java
+++ src/main/java/controller/MentorController.java
@@ -2,19 +2,54 @@ package controller;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+import dao.DBException;
+import dao.MentorDAO;
+import dao.StudentDAO;
+import dao.UserDAO;
 import model.users.Mentor;
+import model.users.Student;
+import model.users.User;
+import org.jtwig.JtwigModel;
+import org.jtwig.JtwigTemplate;
 
 import java.io.IOException;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MentorController implements HttpHandler {
 
-
-
     private Mentor mentor;
+    UserDAO userDAO = new UserDAO();
+    MentorDAO mentorDAO = new MentorDAO();
+    StudentDAO studentDAO = new StudentDAO();
 
 
     public void handle(HttpExchange httpExchange) throws IOException {
 
+        int id = 4;
+        try {
+            String uri = httpExchange.getRequestURI().toString();
+            if (uri.equals("/mentor/store")) {
+            }
+
+            if (uri.equals("/mentor/quests")) {
+            }
+
+            if (uri.equals("/mentor/students")) {
+                showMyStudents(httpExchange);
+            }
+
+            if (uri.equals("/mentor")) {
+                showProfile(httpExchange, id);
+
+            } else {
+                this.showProfile(httpExchange, id);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("IOException in StudentController handle()");
+        }
     }
 
 
@@ -24,5 +59,69 @@ public class MentorController implements HttpHandler {
 
     private void delete() {
 
+    }
+
+    private void showMyStudents(HttpExchange httpExchange){
+        List<Student> studentsList = new ArrayList<>();
+        try {
+            studentsList = studentDAO.getStudentListFromRoom(2);
+        }catch (DBException dbexc) {
+            System.out.println("this is db exception");
+        }
+
+        String userAgent = httpExchange.getRequestHeaders().getFirst("User-agent");
+        JtwigTemplate template = JtwigTemplate.classpathTemplate("templates/admin/mentorList.twig");
+        JtwigModel model = JtwigModel.newModel();
+        model.with("studentsList", studentsList);
+
+        String response = template.render(model);
+        sendResponse(httpExchange, response);
+
+    }
+
+    private void showProfile(HttpExchange httpExchange, int id){
+        JtwigTemplate template = JtwigTemplate.classpathTemplate("templates/mentor/profileMentor.twig");
+        JtwigModel model = JtwigModel.newModel();
+
+        User user = getUser(id);
+
+        String firstName = user.getFirstName();
+        String lastName = user.getLastName();
+        String phoneNumber = user.getPhoneNum();
+        String email = user.getEmail();
+        System.out.println("first name" + firstName);
+        System.out.println("last name" + lastName);
+
+
+        // fill the model with values
+        model.with("firstName", firstName);
+        model.with("lastName", lastName);
+        model.with("phoneNum", phoneNumber);
+        model.with("email", email);
+        String response = template.render(model);
+
+        sendResponse(httpExchange, response);
+    }
+
+    private User getUser(int id){
+        User user = null;
+        try {
+            user = userDAO.seeProfile(id);
+        }catch (DBException dbExc){
+            System.out.println("There was no such user in DB");
+
+        }
+        return user;
+    }
+
+    private void sendResponse(HttpExchange httpExchange, String response) {
+        try{
+            httpExchange.sendResponseHeaders(200, response.length());
+            OutputStream os = httpExchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();}
+        catch (IOException IOExc) {
+            System.out.println("Exception in admin controller");
+        }
     }
 }
