abbd031ab61389c437212a3214f4f522779345fa
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-09-02 13:36:30 +0200

    Add test to check if parsed data is not null

---
+++ src/main/test/helpers/DataParserTest.java
@@ -0,0 +1,26 @@
+package helpers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import javax.xml.crypto.Data;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DataParserTest {
+
+
+    @Test
+    void checkIfParsedDataIsNotNull() {
+        String strToParse = "user1=userowski1&user2=userowski2";
+        try {
+            assertNotNull(DataParser.parseFormData(strToParse));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
