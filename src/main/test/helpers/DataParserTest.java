5733aae4bbdd509a94987183047f5eb8da0da0fe
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-09-02 13:46:27 +0200

    Add test to check if execption is thrown when string to parse is empty

--- src/main/test/helpers/DataParserTest.java
+++ src/main/test/helpers/DataParserTest.java
@@ -1,11 +1,13 @@
 package helpers;
 
+import dao.DBException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import javax.xml.crypto.Data;
 
 import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -21,6 +23,12 @@ class DataParserTest {
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
+    }
+
+    @Test
+    void throwIllegalArgumentExceptionWhenStringToParseIsEmpty() {
+        String strToParse = "";
+        assertThrows(IllegalArgumentException.class, () -> DataParser.parseFormData(strToParse));
     }
 
 }
