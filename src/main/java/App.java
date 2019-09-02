94b9bf77380e64d2c7b57a4fc322e4a0fc5448fe
CzajaKrzysztof <krzysiekczaja@gmail.com>
2019-09-02 10:14:02 +0200

    Change connection port number

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,7 +8,7 @@ import java.net.InetSocketAddress;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        HttpServer server = HttpServer.create(new InetSocketAddress(8040), 0);
+        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
 
         server.createContext("/static", new Static());
         server.createContext("/student", new StudentController());
